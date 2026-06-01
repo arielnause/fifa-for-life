a0c3de0dcb100784dba9f21bf0a52ea43747a677
arielnause <ariel.nause@gmail.com>
2026-06-01 12:34:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'fifa-for-life-v1';
-const ASSETS = ['/', '/index.html', '/app.js', '/manifest.json'];
+const ASSETS = ['./', './index.html', './app.js', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
